58c39e62f57658b79b69f4e08991f07d6b73c5be
DhenSouza <denilson_contato@outlook.com>
2022-03-27 14:26:08 -0300

    Inserindo a entidade Permissao e mapeando a mesma

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,9 @@ insert into cozinha (id, nome) values (2, 'Indiana');
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Douglas restaliv', 3.9, 1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Joao marmitas', 13.9, 2);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Joao tailandesa', 44.9, 1);
+
+insert into formaPagamento (descricao) values ('Cartão Debito');
+insert into formaPagamento (descricao) values ('Cartão Credito');
+insert into formaPagamento (descricao) values ('A vista');
+
+insert into permissao (nome, descricao) values ('Consultar Produtos', 'Status liberado para a consulta de produtos');
