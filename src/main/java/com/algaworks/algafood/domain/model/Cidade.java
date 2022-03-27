fecac90daea2e1b2cfd6826d9e1e38096dd55fb0
DhenSouza <denilson_contato@outlook.com>
2022-03-27 14:46:41 -0300

    incluindo relacionamento ManytoOne para estado

--- src/main/java/com/algaworks/algafood/domain/model/Cidade.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,5 +22,8 @@ public class Cidade {
 
 	@Column(nullable = false)
 	private String nome;
+	
+	@ManyToOne
+	private Estado estado;
 
 }
