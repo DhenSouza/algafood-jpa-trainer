4c9f80dbfe0950ee51ce16b24afc8deea0d647fe
DhenSouza <denilson_contato@outlook.com>
2022-04-30 14:16:55 -0300

    colocando critica na resposta HTTP no metodo buscarPorId

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,6 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,9 +23,15 @@ public class CozinhaController {
 	public List<Cozinha> listar() {
 		return cozinhaRepo.todos();
 	}
-	
+
 	@GetMapping(path = "/buscar/{id}")
 	public ResponseEntity<Cozinha> buscarPorId(@PathVariable Long id) {
-		return ResponseEntity.ok(cozinhaRepo.buscarPorId(id));
+		Cozinha cozinha = cozinhaRepo.buscarPorId(id);
+
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 }
