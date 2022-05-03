93e26013e87c75c3b5887c3dd1e23ec8c79b9699
DhenSouza <denilson_contato@outlook.com>
2022-05-02 21:33:19 -0300

    Subindo primeiro endpoint restaurante Controller

---
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+
+@RestController
+@RequestMapping(value = "/restaurantes")
+public class RestauranteController {
+
+	@Autowired
+	private CadastroRestauranteService service;
+	
+	@GetMapping("/listar")
+	public ResponseEntity<List<Restaurante>> listarRestaurantes() {
+		return ResponseEntity.ok(service.listar());
+	}
+	
+	
+}
