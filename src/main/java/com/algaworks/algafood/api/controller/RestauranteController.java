c9bdc3ca131c567c08a4e775203b217d384116d8
DhenSouza <denilson_contato@outlook.com>
2022-05-02 21:40:23 -0300

    Subindo metodo buscar por id em restauranteController

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,19 @@ public class RestauranteController {
 
 	@Autowired
 	private CadastroRestauranteService service;
-	
+
 	@GetMapping("/listar")
 	public ResponseEntity<List<Restaurante>> listarRestaurantes() {
 		return ResponseEntity.ok(service.listar());
+	}
+
+	@GetMapping("/buscar/{id}")
+	public ResponseEntity<Restaurante> buscarRestaurantePorId(@PathVariable Long id) {
+		Restaurante restaurante = service.buscarPorId(id);
+		if(restaurante != null) {
+			return ResponseEntity.ok(restaurante);
+		}
+		return ResponseEntity.notFound().build();
 	}
 	
 	
