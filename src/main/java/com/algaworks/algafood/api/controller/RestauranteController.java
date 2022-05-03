c960598ce816f83c0576283d84d540d12b75b3f9
DhenSouza <denilson_contato@outlook.com>
2022-05-02 21:45:37 -0300

    Subindo metodo inserirRestaurante por id em restauranteController

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -3,9 +3,12 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +36,8 @@ public class RestauranteController {
 		return ResponseEntity.notFound().build();
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<Restaurante> inserirRestaurante(@RequestBody Restaurante restaurante) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.salvar(restaurante));
+	}
 }
