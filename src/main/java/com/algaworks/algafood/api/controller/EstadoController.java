5e8e862a902deb2aebe73b5d15ec02b3d7d91b5e
DhenSouza <denilson_contato@outlook.com>
2022-05-09 22:01:39 -0300

    inserindo o CRUD em created de estado

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -3,9 +3,12 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +35,11 @@ public class EstadoController {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(service.buscarPorId(id));
+	}
+
+	@PostMapping
+	public ResponseEntity<Estado> incluir(@RequestBody Estado estado) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.salvar(estado));
 	}
 
 }
