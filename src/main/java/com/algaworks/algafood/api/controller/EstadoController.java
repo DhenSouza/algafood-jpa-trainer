71e15f4dc4549fa7c399ec295857eb3efc1f50b9
DhenSouza <denilson_contato@outlook.com>
2022-05-09 22:06:10 -0300

    inserindo o CRUD em alter em estado

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,14 @@ public class EstadoController {
 	@PostMapping
 	public ResponseEntity<Estado> incluir(@RequestBody Estado estado) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.salvar(estado));
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Estado> alterar(@RequestBody Estado estado, @PathVariable Long id) {
+		if(estado == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(service.alterar(estado, id));
 	}
 
 }
