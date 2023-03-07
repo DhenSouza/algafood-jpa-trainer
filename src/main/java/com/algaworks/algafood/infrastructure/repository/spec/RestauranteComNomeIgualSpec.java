5cf6ffeb888041c8cc7e756c788d1a51ff745ab8
denilson.souza <denilson.souza@original.com.br>
2023-03-07 10:40:09 -0300

    feat(specification): Realizado a criação das classes specifications para a implementação da consulta

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/spec/RestauranteComNomeIgualSpec.java
@@ -0,0 +1,18 @@
+package com.algaworks.algafood.infrastructure.repository.spec;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public class RestauranteComNomeIgualSpec implements Specification<Restaurante>{
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public Predicate toPredicate(Root<Restaurante> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+        return null;
+    }
+}
