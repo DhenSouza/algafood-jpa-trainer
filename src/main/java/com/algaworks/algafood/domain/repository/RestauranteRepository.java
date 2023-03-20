1468a330f85d16560f3de827f518f5baecc7f132
denilson.souza <denilson.souza@original.com.br>
2023-03-20 14:41:07 -0300

    feat: Implementando solução n+1 das consultas e as regras lazy = to many e eager = toMany

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -13,6 +13,12 @@ import com.algaworks.algafood.domain.model.Restaurante;
 
 @Repository
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
+
+    /* O segundo Join so resolve o problema do n+1 de toMany, usando o left join fech pois, se fosse so o join o modo seria EASY e nao EAGER (hibernate deixa automatico)*/
+    // Caso queira a solução definitiva do n+1, somente personalizando a query com JPQL caso use o JPA
+    @Query("FROM Restaurante r JOIN FETCH r.cozinha LEFT JOIN FETCH r.formasPagamento")
+    List<Restaurante> findAll();
+
 	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id ")
     List<Restaurante> consultaPorNome(String nome, @Param("id") Long cozinha);
 
